d5bbfff64637e3514038c1bf105307ec8527c9c5
marco julio aparicio <mjaf83@gmail.com>
2025-05-12 18:21:18 +0100

    Added Firecrawl

--- ingestion.py
+++ ingestion.py
@@ -35,10 +35,50 @@ def ingest_docs():
             index_name="langchain-doc-index",
         )
 
+def ingest_docs_2() -> None:
+    from langchain_community.document_loaders import FireCrawlLoader
+
+    langchain_docs_base_urls= [
+        "https://python.langchain.com/docs/integrations/chat/",
+        "https://python.langchain.com/docs/integrations/llms/",
+        "https://python.langchain.com/docs/integrations/memory/",
+        "https://python.langchain.com/docs/integrations/stores/",
+        "https://python.langchain.com/docs/integrations/text_embedding/",
+        "https://python.langchain.com/docs/integrations/document_loaders/",
+        "https://python.langchain.com/docs/integrations/document_transformers/",
+        "https://python.langchain.com/docs/integrations/verctorstores/",
+        "https://python.langchain.com/docs/integrations/retrievers/",
+        "https://python.langchain.com/docs/integrations/tools/",
+        "https://python.langchain.com/docs/integrations/llm_caching/",
+        "https://python.langchain.com/docs/integrations/graphs/",
+        "https://python.langchain.com/docs/integrations/callbacks/",
+        "https://python.langchain.com/docs/integrations/chat_loaders/",
+        "https://python.langchain.com/docs/integrations/concepts/"
+    ]
+    for url in langchain_docs_base_urls:
+        print(f"firecrawling: {url=}")
+        loader = FireCrawlLoader(
+            url=url,
+            mode="crawl",
+            params = {
+                #"crawlerOptions": {"limit": 5 },
+                #"pageOptions": {"onlyMainContent": True},
+                #"wait_until_done": True
+            },
+        )
+        docs = loader.load()
+        print(f"Going to add {len(docs)} documents to Pinecone")
+        print(f"Document loaded: {url} to vectorstore")
+        PineconeVectorStore.from_documents(
+            docs,
+            embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+            index_name="firecrawl-index",
+        )
+
 if __name__ == "__main__":
     print("Loading documents...")
-    ingest_docs()
-
+    #ingest_docs()
+    ingest_docs_2()
     #llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
 
 
